bcb7ab7414c29cb0ce945ce517787b07ebbb2607
TEAM ISTKHAR <mdistkhar1078@gmail.com>
2024-07-30 06:18:01 +0530

    Update play.py

--- VeenaXMusic/utils/inline/play.py
+++ VeenaXMusic/utils/inline/play.py
@@ -2,7 +2,7 @@ import math
 
 from pyrogram.types import InlineKeyboardButton
 
-from veenaqueen.utils.formatters import time_to_seconds
+from VeenaXMusic.utils.formatters import time_to_seconds
 
 
 def track_markup(_, videoid, user_id, channel, fplay):
