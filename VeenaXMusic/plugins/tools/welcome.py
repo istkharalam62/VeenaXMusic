c73c75073dc9532a03b38f4a85ca29ffa075c923
TEAM ISTKHAR <mdistkhar1078@gmail.com>
2024-07-30 06:21:54 +0530

    Update welcome.py

--- VeenaXMusic/plugins/tools/welcome.py
+++ VeenaXMusic/plugins/tools/welcome.py
@@ -16,7 +16,7 @@ from asyncio import sleep
 from pyrogram import filters, Client, enums
 from pyrogram.enums import ParseMode
 from logging import getLogger
-from VeenaXMusic.utils.veena_ban import admin_filter
+from VeenaXMusic.utils.VeenaXMusic_ban import admin_filter
 from PIL import ImageDraw, Image, ImageFont, ImageChops
 from pyrogram import *
 from pyrogram.types import *
